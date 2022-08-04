bd7dd34d85dcda322acced3e176b4b4b1922441c
Lucas Dantas <lucasmbrute614@gmail.com>
2022-08-04 19:47:50 -0300

    feat: implemented middleware to handle with exepctions

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,10 +1,26 @@
 import "reflect-metadata"
-import express from "express"
+import express, { Request, Response, NextFunction } from "express"
+import "express-async-errors"
 import dontenvConfigs from "../../configs/dontenvEntries"
+import "../infra/container"
 import { createConnection } from "../infra/database/data-source"
+import { AppError } from "../errors/AppError"
 const app = express()
 
 app.use(express.json())
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      message: err.message
+    })
+  }
+
+  return res.status(500).json({
+    status: "error",
+    message: `Internal server error - ${err.message}`
+  })
+})
+
 createConnection()
 
 app.listen(dontenvConfigs.server.port, () => {
