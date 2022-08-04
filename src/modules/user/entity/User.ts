35897142ba29ee3323fdb9d5f77fa9e9aa9bfefd
Lucas Dantas <lucasmbrute614@gmail.com>
2022-08-04 15:18:20 -0300

    feat: Added typeorm entity sintaxy into User entity

--- src/modules/user/entity/User.ts
+++ src/modules/user/entity/User.ts
@@ -1,10 +1,22 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 import { v4 as uuid } from "uuid"
 
+@Entity()
 export class User {
+
+  @PrimaryGeneratedColumn()
   id?: string;
+
+  @Column()
   name!: string;
+
+  @Column()
   email!: string;
+
+  @Column()
   birthday!: Date;
+
+  @Column()
   phone!: string;
 
   constructor() {
