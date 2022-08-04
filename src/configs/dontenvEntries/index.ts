3ee2325fd1307f639b1d52ba41b30eac92874b1f
Lucas Dantas <lucasmbrute614@gmail.com>
2022-08-04 14:49:47 -0300

    chore: implemeneted Postgres BD into docker compose and added username and password into dotenv

--- src/configs/dontenvEntries/index.ts
+++ src/configs/dontenvEntries/index.ts
@@ -4,5 +4,9 @@ dotenv.config()
 export default {
   server: {
     port: process.env.PORT ?? 3131
+  },
+  datanase: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD
   }
 }
