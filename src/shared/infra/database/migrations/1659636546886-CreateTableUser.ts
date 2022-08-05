7dfb86403a6a5961ad7686af3486f54e0dd063d5
Lucas Dantas <lucasmbrute614@gmail.com>
2022-08-05 03:50:42 -0300

    fix: id as numeric instead uuid and changed identation

--- src/shared/infra/database/migrations/1659636546886-CreateTableUser.ts
+++ src/shared/infra/database/migrations/1659636546886-CreateTableUser.ts
@@ -3,33 +3,34 @@ import { MigrationInterface, QueryRunner, Table } from "typeorm"
 export class CreateTableUser1659636546886 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.createTable(new Table({
-      name: "user",
-      columns: [
-        {
-          name: "id",
-          type: "uuid",
-          isPrimary: true
-        },
-        {
-          name: "name",
-          type: "varchar",
-        },
-        {
-          name: "email",
-          type: "varchar",
-          isUnique: true
-        },
-        {
-          name: "birthday",
-          type: "varchar"
-        },
-        {
-          name: "phone",
-          type: "numeric"
-        }
-      ]
-    }))
+    await queryRunner.createTable(
+      new Table({
+        name: "user",
+        columns: [
+          {
+            name: "id",
+            type: "numeric",
+            isPrimary: true
+          },
+          {
+            name: "name",
+            type: "varchar",
+          },
+          {
+            name: "email",
+            type: "varchar",
+            isUnique: true
+          },
+          {
+            name: "birthday",
+            type: "varchar"
+          },
+          {
+            name: "phone",
+            type: "numeric"
+          }
+        ]
+      }))
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
